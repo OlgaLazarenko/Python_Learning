90ee45ae5d43801109d6a0a3eb59cbe7d120a393
OlgaLazarenko <olazarenko@outlook.com>
2020-12-01 21:16:49 -0500

    first commit

---
+++ Python_List.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+'''
+Project Name: Companies Annual Chargeable_Income (2007-2018)
+Part 1: Chargeable_Income
+Date: Nov 22, 2020
+Author: Olga Lazarenko
+Description: 
+'''
+print("this is my new project")
