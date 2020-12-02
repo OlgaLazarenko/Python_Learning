3275243da84c16d62d8661b33578705539c72018
Olga Lazarenko <olazarenko@outlook.com>
2020-12-02 01:04:05 -0500

    first commit

---
+++ Python_Pandas.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+'''
+Project Name: Python_Pandas_Learning
+Date started: Dec 01, 2020
+Author: Olga Lazarenko
+Description: the purpose of the project is to get more familiar and 
+             enhance skills at Pandas package:
+             - work with series
+             - work with DataFrame
+             
+
+'''
+print("Started on Dec 02, 2020")
