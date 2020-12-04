0a783193f7694512cfaec4cffc08b98f53f5ddbb
Olga Lazarenko <olazarenko@outlook.com>
2020-12-04 00:37:55 -0500

    create DataFrome from a single list

--- Python_Pandas.py
+++ Python_Pandas.py
@@ -12,3 +12,12 @@ Description: the purpose of the project is to get more familiar and
 
 '''
 print("Started on Dec 02, 2020")
+
+import pandas as pd
+import numpy as np
+
+# create a DataFrame using a single list/ or list of lists
+cars_list = ['Ford','Honda','Chevy','Nissan','Kia']
+df_cars = pd.DataFrame(cars_list)
+print('df_cars')
+print(df_cars)
