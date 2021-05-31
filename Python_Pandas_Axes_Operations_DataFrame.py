255112c65a8ab3929e5db845bfd6a81e5854d2f2
Olga Lazarenko <olazarenko@outlook.com>
2021-05-30 23:47:47 -0400

    concatenante dataframes, axis=1

--- Python_Pandas_Axes_Operations_DataFrame.py
+++ Python_Pandas_Axes_Operations_DataFrame.py
@@ -70,3 +70,9 @@ auto_volvo_2 = auto_volvo[["Symboling" , "Normalized Loss" , "City mpg" , "Highw
 print()
 print("auto_volvo_2")
 print(auto_volvo_2)
+print()
+
+# combine the dataframes <auto_volvo_1> and <auto_volvo_2)
+volvo_result_ax1 = pd.concat([auto_volvo_1, auto_volvo_2], axis = 1)
+print('concatenated dataframes , axis=1')
+print(volvo_result_ax1)
