61285c07fba3f9c772b59e7daf66b083b8263e8c
Olga Lazarenko <olazarenko@outlook.com>
2021-06-02 23:45:32 -0400

    filter DataFrame

--- Python_Pandas_Filter_DataFrame.py
+++ Python_Pandas_Filter_DataFrame.py
@@ -17,3 +17,9 @@ auto = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\A
 print()
 print("<auto> DataFrame")
 print(auto.head())
+
+# 1) Filter the data, DataFrame way
+new_auto_1 = auto[auto["Make"] == "toyota"]
+print()
+print("<new_auto_1>  DataFrame")
+print(new_auto_1)
