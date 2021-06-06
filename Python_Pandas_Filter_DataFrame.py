71f54e0086da813cb1c2b404c3b8a00532b1cc6a
Olga Lazarenko <olazarenko@outlook.com>
2021-06-05 23:48:06 -0400

    some changes

--- Python_Pandas_Filter_DataFrame.py
+++ Python_Pandas_Filter_DataFrame.py
@@ -17,7 +17,11 @@ auto = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\A
 print()
 print("<auto> DataFrame")
 print(auto.head())
+auto["Make"].astype(str)
+auto["Body Style"].astype(str)
+print(auto.dtypes)
 
+'''
 # 1) Filter the data, DataFrame way
 new_auto1_toyota = auto[auto["Make"] == "toyota"]
 print()
@@ -25,24 +29,16 @@ print("<new_auto1_toyota>  DataFrame")
 print(new_auto1_toyota)
 print()
 print()
-new_auto1_bmw_volvo = auto[ (auto["Make"] == "bmw") & (auto["Make"] == "volvo")]
-print("<new_auto1_bmw_volvo>  DataFrame")
-print(new_auto1_bmw_volvo)
+bmw_volvo = auto[ (auto["Make"] == "bmw") & (auto["Make"] == "volvo")]
+print("bmw_volvo>  DataFrame")
+print(bmw_volvo)
+'''
 
 # <Empty DatFrame> notification is returned 
 # check the columns data type
-print("columns data type")
-print(auto.dtypes)
-print()
-print()
+
 # convert <Make> column data type from object to string
 # change the columns data types to appropirate ones
-convert_dict = { 
-                "Normalized Loss" : str ,
-                "Make" : str ,
-                "Body Style" : str}
 
-auto = auto.astype(convert_dict)
 
-print("converted  data type")
-print(auto.dtypes)
+
