9b6d9e9aa2d0a7ab3e049f19fcc8102118c34af7
Olga Lazarenko <olazarenko@outlook.com>
2021-06-21 22:47:53 -0400

    rename columns

--- Read_Write_Files.py
+++ Read_Write_Files.py
@@ -14,6 +14,30 @@ for rows in auto.columns:
     print(rows)
 print()
 
+# rename columns of <auto> data frame
+auto.rename( columns = {
+                        'Fuel Type' : 'Fuel_Type' ,
+                        'Num of Doors' : 'Num_of_Doors' ,
+                        'Body Style' : 'Body_Style' ,
+                        'Drive Wheels' : 'Drive_Wheels' ,
+                        'Engine Location' : 'Engine_Location' ,
+                        'Wheel Base' : 'Wheel_Base' ,
+                        'Engine Type' : 'Engine_Type' , 
+                        'Num of Cylinders' : 'Num_of_Cylinders' , 
+                        'Engine Size' : 'Engine_Size' ,
+                        'Fuel System' : 'Fuel_System' ,
+                        'Compression Ratio' : 'Compression_Ratio' ,
+                        'Peak rmp' : 'Peak_rmp' ,
+                        'City mpg' : 'City_mpg' ,
+                        'Highway mpg' : 'Highway_mpg'
+
+                        }
+            )
+print()
+print()
+print('new column names of <auto> data frame')
+for col in auto:
+    print(col)
 # --------------------------------------------------------------------------------------------------
 # write the data to files
 # df.to_csv('file_name.csv')
